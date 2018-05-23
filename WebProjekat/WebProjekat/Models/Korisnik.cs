ab9f88bbb7e80752bf1c86c440cd4875c359c415
Mario996 <klisanicmario@gmail.com>
2018-05-23 21:03:26 +0200

    dodane pocetne klase

---
+++ WebProjekat/WebProjekat/Models/Korisnik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProjekat
+{
+    public class Korisnik
+    {
+        static int brojacKorisnika = 0;
+
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public PolOsobe Pol { get; set; }
+        public string JMBG { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+        public TipVoznje TipVoznje { get; set; }
+
+        public Korisnik() {
+            brojacKorisnika++;
+            ID = brojacKorisnika;
+        }
+    }
+}
---
+++ WebProjekat/WebProjekat/Models/Lokacija.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProjekat
+{
+    public class Lokacija
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Adresa Adresa { get; set; }
+    }
+}
